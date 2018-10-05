690999a8c8e7cab4fd89ee8ebe86749d1b8a384c
George Gach <george@gach.space>
2018-10-05 18:44:03 -0400

    Update cross.py

--- cross.py
+++ cross.py
@@ -5,27 +5,29 @@ class Crosshair(QtWidgets.QWidget):
     def __init__(self, parent=None, windowSize=24, penWidth=2):
         QtWidgets.QWidget.__init__(self, parent)
         self.ws = windowSize
-        self.resize(windowSize, windowSize)
+        self.resize(windowSize+1, windowSize+1)
         self.pen = QtGui.QPen(QtGui.QColor(100,255,100,150))                
         self.pen.setWidth(penWidth)                                            
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.WindowTransparentForInput)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-        self.move(QtWidgets.QApplication.desktop().screen().rect().center() - self.rect().center())
+        self.move(QtWidgets.QApplication.desktop().screen().rect().center() - self.rect().center() + QtCore.QPoint(1,1))
 
 
     def paintEvent(self, event):
+        ws = self.ws
+        d = 6
         painter = QtGui.QPainter(self)
         painter.setPen(self.pen)
-        # painter.drawLine(x1,y1, x2,y2)
-        painter.drawLine(self.ws/2, 0, self.ws/2, self.ws/2-self.ws/6)          # Top
-        painter.drawLine(self.ws/2, self.ws/2+self.ws/6, self.ws/2, self.ws)    # Bottom
-        painter.drawLine(0, self.ws/2, self.ws/2 - self.ws/6, self.ws/2)        # Left
-        painter.drawLine(self.ws/2 + self.ws/6, self.ws/2, self.ws, self.ws/2)  # Right
+        # painter.drawLine( x1,y1, x2,y2    )
+        painter.drawLine(   ws/2, 0,               ws/2, ws/2 - ws/d   )   # Top
+        painter.drawLine(   ws/2, ws/2 + ws/d,     ws/2, ws            )   # Bottom
+        painter.drawLine(   0, ws/2,               ws/2 - ws/d, ws/2   )   # Left
+        painter.drawLine(   ws/2 + ws/d, ws/2,     ws, ws/2            )   # Right
 
 
 app = QtWidgets.QApplication(sys.argv) 
 
-widget = Crosshair(windowSize=24, penWidth=2)
+widget = Crosshair(windowSize=24, penWidth=1)
 widget.show()
 
 sys.exit(app.exec_())
